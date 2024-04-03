20a2cc1da9b867c2e099611e2f90892b522a3f62
Alisdghdjsksj <karamianiali99@gmail.com>
2024-04-03 12:17:29 +0330

    tamrin1

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("enter number");
+int x=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("enter number");
+int y=Convert.ToInt32(Console.ReadLine());
+int z = x + y;
+System.Console.WriteLine("sum of two numbers is "+z);
